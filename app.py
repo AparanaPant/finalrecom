aa3f6b08b467ab7aa46c04cd574babd297a005f9
apapant <aparana.pant@jeeveehealth.com.np>
2021-08-03 01:11:42 +0545

    fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from data_preocessing import item_similarity, popularity
 app = flask.Flask(__name__)
 
 
-@app.route('/home', methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
     user_id = request.args['user_id']
     response = item_similarity(user_id)
@@ -15,13 +15,13 @@ def home():
     return js
 
 
-@app.route('/popularity', methods=['GET'])
-def popularity():
-    user_id = request.args['user_id']
-    response = popularity(user_id)
-    print(response)
-    js = response['song'].to_json()
-    return js
+# @app.route('/popularity', methods=['GET'])
+# def popularity():
+#     user_id = request.args['user_id']
+#     response = popularity(user_id)
+#     print(response)
+#     js = response['song'].to_json()
+#     return js
 
 
 # if __name__ == '__main__':
